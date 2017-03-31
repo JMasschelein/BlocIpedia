a49c145b72220f7a0fc6d471aeb4bac4880ec105
Jeroen Masschelein <jeroen.masschelein@gmail.com>
2017-03-31 14:18:28 -0400

    checkpoint 1 6

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+    
+    has_many :wikis, dependent: :destroy
+    
+after_create :send_admin_mail
+def send_admin_mail
+  UserMailer.send_welcome_email(self).deliver_later
+end    
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
+  resources :wikis
+
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 devise_for :users, controllers: {
     sessions: 'users/sessions'
       }    
     
-  get 'welcome/index'
-
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
  root 'welcome#index'
 end
